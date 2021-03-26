45972e2aa1f7ef001e77318e35bc63be6e3152be
austin kwinta <arkwinta@gmail.com>
2021-03-25 23:20:23 -0400

    Adds a bit more content to the homepage, updates input to be numeric with limits

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -22,10 +22,19 @@ export default class Content extends React.Component {
     render() {
         return (
             <div>
+                <h1> Welcome to Pokepaths! </h1>
+                <h3> let's get you home </h3>
                 <p>
                     What are the dimensions of your map?
                 </p>
-                <input value={this.state.length} onChange={event => this.handleDimensionUpdate(event)} className="length-input" />
+                <input
+                    value={this.state.length}
+                    onChange={event => this.handleDimensionUpdate(event)}
+                    className="length-input"
+                    type="number"
+                    min="1"
+                    max="100"
+                />
                 <Board length={this.state.length} />
             </div>
         )
